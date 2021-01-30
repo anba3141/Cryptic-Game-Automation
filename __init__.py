c7ba0c8b4c133e6fd78de2b879b137755ab5fb63
anba3141 <35229590+anba3141@users.noreply.github.com>
2021-01-31 00:41:13 +0100

    added main file

---
+++ __init__.py
@@ -0,0 +1,109 @@
+from selenium import webdriver
+import time
+import random
+
+
+driver = webdriver.Chrome('/chromedriver')
+driver.get("https://cryptic-game.net")
+
+
+def timer(a: float, b: float):
+    time.sleep(random.randint(int(a*10000), int(b*10000))/10000)
+
+
+def login(username, password):
+    time.sleep(random.randint(1, 3))
+    play_button = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/nav/div[1]')
+    try:
+        play_button.click()
+    except:
+        pass
+
+    timer(1, 3)
+    driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/nav/div[2]/div/div/a[2]').click()
+
+    timer(1, 3)
+
+    driver.switch_to.window(driver.window_handles[-1])
+
+    username_login = driver.find_element_by_xpath('//*[@id="animation-container"]/app-login/app-account-page-base/div/div[1]/div/form/label[1]/input')
+    passwort_login = driver.find_element_by_xpath('//*[@id="animation-container"]/app-login/app-account-page-base/div/div[1]/div/form/label[2]/input')
+    timer(1, 5)
+    username_login.send_keys(username)
+    timer(2, 5)
+    passwort_login.send_keys(password)
+    timer(2, 5)
+    driver.find_element_by_xpath('//*[@id="animation-container"]/app-login/app-account-page-base/div/div[1]/div/form/label[3]/input').click()
+    time.sleep(2)
+
+
+def open_pc(pc: int):
+    driver.find_element_by_xpath('//*[@id="sidebar-container"]/app-control-center-sidebar-menu[1]').click()
+    timer(1, 2)
+    driver.find_element_by_xpath(f'//*[@id="sidebar-container"]/app-control-center-sidebar-menu[1]/div/div[2]/div[{pc}]').click()
+    timer(1, 2)
+    driver.find_element_by_xpath('//*[@id="control-center"]/div/div[2]/div/app-control-center-device-page/div[1]/img[1]').click()
+
+
+def open_terminal():
+    timer(1, 2)
+    app_button = driver.find_element_by_xpath('//*[@id="startbutton"]')
+    app_button.click()
+    timer(1, 2)
+    terminal_button = driver.find_element_by_xpath('//*[@id="linkages"]/div[3]')
+    terminal_button.click()
+
+
+class Command:
+    def __init__(self, command: str):
+        self.command = command
+        self.is_send = False
+        self.response = None
+        self.sent = None
+
+    def send(self):
+        timer(4, 6)
+        command_line = driver.find_element_by_xpath('//*[@id="cmdline"]')
+        command_line.send_keys(self.command+"\n")
+        self.is_send = True
+        timer(0.5, 1)
+        self.response = driver.find_element_by_xpath('//*[@id="terminal-history"]').text.split(
+            driver.find_elements_by_xpath('//*[@id="prompt"]')[-1].text)[-1].split("\n")
+        self.sent = self.response.pop(0)
+
+
+def enter_command(cmd: str):
+    timer(3, 7)
+    command_line = driver.find_element_by_xpath('//*[@id="cmdline"]')
+    command_line.send_keys(cmd)
+    timer(1, 2)
+
+
+def submit_command():
+    timer(1, 2)
+    command_line = driver.find_element_by_xpath('//*[@id="cmdline"]')
+    command_line.send_keys("\n")
+    timer(1, 2)
+
+
+def close_terminal():
+    timer(1, 2)
+    while True:
+        try:
+            enter_command("exit")
+            submit_command()
+        except:
+            break
+    timer(1, 2)
+
+
+def select_uuid():
+    timer(1, 2)
+    driver.find_element_by_xpath('//*[@id="terminal-history"]/ul/li[1]/span').click()
+    timer(1, 2)
+
+
+def select_ssh():
+    timer(1, 2)
+    driver.find_element_by_xpath('//*[@id="terminal-history"]/ul/ul/li/span').click()
+    timer(1, 2)
