53f221d9dc6e4684ee5167b3dae55b47984aa3a5
anba3141 <35229590+anba3141@users.noreply.github.com>
2021-01-31 15:29:16 +0100

    fixed error

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ with OpenCryptic("username", "password", "chromedriver path") as cryptic:
                 cmd = terminal.Command(f"service bruteforce {spot_cmd.get_device_uuid()} {spot_cmd.get_device_ssh()}")
                 timer(19, 23)
                 cmd.command = "stop"
-                cmd.command = f"connect {spot_cmd.get_uuid()}"
+                cmd.command = f"connect {spot_cmd.get_device_uuid()}"
                 cmd.command = "miner start c91e8b97-baae-4926-96d4-0d07e5a1c804"
                 cmd.command = "miner wallet c91e8b97-baae-4926-96d4-0d07e5a1c804"
                 cmd.command = "miner power 100"
