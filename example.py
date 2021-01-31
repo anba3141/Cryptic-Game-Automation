c96f9c6573ad11a3e995c426e1188ec5f5fed866
anba3141 <35229590+anba3141@users.noreply.github.com>
2021-01-31 14:48:40 +0100

    changed code to new Version

--- example.py
+++ example.py
@@ -1,27 +1,16 @@
 from cryptic import *
 
-login("username", "password")
-open_pc(2)
-open_terminal()
-hacked = 0
-while True:
-    spot_cmd = Command("spot")
-    spot_cmd.send()
-    cmd = Command("service bruteforce "+get_uuid(spot_cmd)+" "+get_ssh(spot_cmd))
-    cmd.send()
-    cmd = Command("stop")
-    timer(21, 25)
-    cmd.send()
-    cmd = Command("connect "+get_uuid(spot_cmd))
-    cmd.send()
-    cmd = Command("miner start c91e8b97-baae-4926-96d4-0d07e5a1c804")
-    cmd.send()
-    cmd = Command("miner wallet c91e8b97-baae-4926-96d4-0d07e5a1c804")
-    cmd.send()
-    cmd = Command("miner power 100")
-    cmd.send()
-    cmd = Command("exit")
-    cmd.send()
-    timer(0, 2)
-    hacked += 1
-    print(hacked, "people Hacked so far")
+with OpenCryptic("username", "password", "chromedriver path") as cryptic:
+    with cryptic.OpenPc(2) as pc:
+        with pc.OpenTerminal() as terminal:
+            while True:
+                spot_cmd = terminal.Command("spot")
+                cmd = terminal.Command(f"service bruteforce {spot_cmd.get_device_uuid()} {spot_cmd.get_device_ssh()}")
+                timer(19, 23)
+                cmd.command = "stop"
+                cmd.command = f"connect {spot_cmd.get_uuid()}"
+                cmd.command = "miner start c91e8b97-baae-4926-96d4-0d07e5a1c804"
+                cmd.command = "miner wallet c91e8b97-baae-4926-96d4-0d07e5a1c804"
+                cmd.command = "miner power 100"
+                cmd.command = "exit"
+
